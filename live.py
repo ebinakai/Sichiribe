5fc03ba99eb18be965249709a410df3049dfbdf2
EbinaKai <blackbicycle55@gmail.com>
2024-08-30 01:18:31 +0300

    [fix] add export function in live.py

--- live.py
+++ live.py
@@ -13,6 +13,7 @@ import argparse
 from utils.capture import FrameCapture
 from utils.frameEditor import FrameEditor
 from utils.cnn import CNN as Detector
+from utils.exporter import Exporter, get_supported_formats
 from utils.common import get_now_str
 import time
 import os
@@ -20,6 +21,7 @@ import cv2
 
 # クラスのインスタンスを作成してフレームをキャプチャ
 if __name__ == "__main__":
+  export_formats = get_supported_formats()
   
   # 引数を取得
   parser = argparse.ArgumentParser(description='7セグメントディスプレイの数字を読み取る') 
@@ -28,6 +30,7 @@ if __name__ == "__main__":
   parser.add_argument('--num-frames', help="サンプリングするフレーム数", type=int, default=20)
   parser.add_argument('--interval-min', help="サンプリング間隔（分）", type=float, default=1)
   parser.add_argument('--total-sampling-min', help="サンプリングする合計時間（分）", type=float, default=20)
+  parser.add_argument('--format', help="出力形式 (json または csv)", choices=export_formats, default='json')
   parser.add_argument('--save-frame', help="キャプチャしたフレームを保存するか", action='store_true')
   parser.add_argument('--debug', help="デバッグモードを有効にする", action='store_true')
   args = parser.parse_args()
@@ -39,7 +42,8 @@ if __name__ == "__main__":
   
   fc = FrameCapture(device_num=args.device)
   fe = FrameEditor(num_digits=args.num_digits)
-  dt = Detector(args.num_digits)
+  dt = Detector(num_digits=args.num_digits)
+  ep = Exporter(method=args.format)
   
   # 画角を調整するためにカメラフィードを表示
   fc.show_camera_feed()
@@ -52,6 +56,8 @@ if __name__ == "__main__":
   total_duration_seconds = args.total_sampling_min * 60
   end_time = time.time() + total_duration_seconds
   frame_count = 0
+  timestamp = []
+  results = []
   while time.time() < end_time:
     start_time = time.time()
     frames = []
@@ -76,6 +82,8 @@ if __name__ == "__main__":
     if len(frames) != 0:
       value, failed_rate = dt.detect(frames)
       logger.info(f"Detected: {value}, Failed rate: {failed_rate}")
+      results.append(value)
+      timestamp.append(get_now_str())
           
     elapsed_time = time.time() - start_time
     time_to_wait = max(0, interval_sec - elapsed_time)
@@ -85,4 +93,8 @@ if __name__ == "__main__":
       time.sleep(time_to_wait)
   
   fc.release()
+  
+  # 結果のエクスポート
+  data = ep.format(results, timestamp)
+  ep.export(data)
   
