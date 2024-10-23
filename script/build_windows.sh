ee4c03a38b1ce5c4f8eb08afe7c9175ddf7113c4
EbinaKai <blackbicycle55@gmail.com>
2024-10-23 13:53:54 +0300

    [add] build script for windows.

---
+++ script/build_windows.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+nuitka \
+    --product-name=Sichiribe \
+    --enable-plugins=pyside6 \
+    --include-module=matplotlib.backends.backend_qt5agg \
+    --include-data-dir=./model=model \
+    --include-data-dir=./gui/images=gui/images \
+    app.py
