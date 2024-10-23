e1d35a886970182ee7230543656b1cd55c411853
EbinaKai <blackbicycle55@gmail.com>
2024-10-23 17:25:30 +0300

    [fix] Updated model path from keras to onnx.

--- script/build_windows.sh
+++ script/build_windows.sh
@@ -5,8 +5,8 @@ python -m nuitka \
     --windows-icon-from-ico=script/Sichiribe_icon.png \
     --enable-plugins=pyside6 \
     --include-module=matplotlib.backends.backend_qt5agg \
-    --include-data-files=./model/model_100x100.keras=model/model_100x100.keras \
-    --include-data-files=./gui/images/splash_image.png=gui/images/splash_image.png \
+    --include-data-dir=./model=model \
+    --include-data-dir=./gui/images=gui/images/splash_image.png \
     --product-version=1.0.0 \
     --onefile \
     app.py
