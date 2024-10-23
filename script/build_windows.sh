13e0b3d6078482a2ca377d53efc8f9b325046cf8
EbinaKai <blackbicycle55@gmail.com>
2024-10-23 14:35:37 +0300

    [fix] 'data files cannot be included in accelerated mode' by nuitka

--- script/build_windows.sh
+++ script/build_windows.sh
@@ -5,6 +5,6 @@ python -m nuitka \
     --enable-plugins=pyside6 \
     --product-version=1.0.0 \
     --include-module=matplotlib.backends.backend_qt5agg \
-    --include-data-dir=./model=model \
+    --include-data-file=./model/model_100x100.keras=model_100x100.keras \
     --include-data-dir=./gui/images=gui/images \
     app.py
