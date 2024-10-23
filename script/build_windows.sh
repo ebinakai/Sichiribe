2bff2f2428e162d722d1bac4660d7de4899d8073
EbinaKai <blackbicycle55@gmail.com>
2024-10-23 14:25:07 +0300

    [fix] AssertionError by nuitka.

--- script/build_windows.sh
+++ script/build_windows.sh
@@ -3,6 +3,7 @@
 python -m nuitka \
     --product-name=Sichiribe \
     --enable-plugins=pyside6 \
+    --product-version=1.0.0 \
     --include-module=matplotlib.backends.backend_qt5agg \
     --include-data-dir=./model=model \
     --include-data-dir=./gui/images=gui/images \
